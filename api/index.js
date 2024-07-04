70bcb75164611ad6d992566408ab41128fab7493
Feminask <fmnzrasheed@gmail.com>
2024-07-04 15:58:31 +0530

    test api

--- api/index.js
+++ api/index.js
@@ -1,23 +1,24 @@
-import { log } from "console";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
 mongoose
-  .connect(
-    process.env.MONGO)
+  .connect(process.env.MONGO)
   .then(() => {
-    console.log('MongoDb is connected');
+    console.log("MongoDb is connected");
   })
   .catch((err) => {
     console.log(err);
   });
 
-const app = express()
+const app = express();
 
 const PORT = 9000;
-app.listen(PORT,()=>{
-    console.log(`server running on ${PORT}..!.`);
-})
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}..!.`);
+});
+
+app.use("/api/user", userRoutes);
