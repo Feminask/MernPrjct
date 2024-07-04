e207d2dcf1c57d8787e34e196e72f1b3cfb4410f
Feminask <fmnzrasheed@gmail.com>
2024-07-04 16:21:59 +0530

    add middleware and handle errors

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
+
 
 const PORT = 9000;
 app.listen(PORT, () => {
@@ -22,3 +25,17 @@ app.listen(PORT, () => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+
+
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode || 500
+  const message = err.message || 'Internal Server Error'
+  res.status(statusCode).json({
+      success:false,
+      statusCode,
+      message
+  })
+})
+
